c8b8a00edfa916c8a8ad292608c41e78c585fc32
Ens10-37 <nkaila25@students.oamk.fi>
2026-04-02 02:17:18 +0300

    Käyttäjä istunnot, rekisteröinti, kirjautuminen

--- backend/Server/index.js
+++ backend/Server/index.js
@@ -1,58 +1,124 @@
-require('dotenv').config();
-const express = require('express');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-const pool = require('./helpers/db');
+require('dotenv').config()
+const express = require('express')
+const session = require('express-session')
+const pgSession = require('connect-pg-simple')(session)
+const pool = require('./helpers/db')
+const { registerUser, loginUser } = require('./helpers/authHjelper')
+const path = require('path')
 
-const app = express();
-const port = 3001
+const app = express()
+const port = 3000
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../../frontend')))
+app.use(express.urlencoded({ extended: true }))
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'keyboard cat',
+    store: new pgSession({
+        pool : pool,
+        tableName : 'session'
+    }),
+  secret: process.env.SESSION_SECRET || 'mon petite chou-fleur', //Saa laitta oman
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: false } // Set to true if using HTTPS
-}));
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secure: false 
+  }
+}))
 
-//Routes
-app.post('/register', async (req, res) =>{
-    const { username, password } = req.body;
-    try{
-        const hashedPassword = await bcrypt.hash(password, 10);
-        
-        await pool.query(
-            'INSERT INTO users (username, password) VALUES ($1, $2)',
-            [username, hashedPassword]
-        );
-        res.redirect('/login');
-    } catch (err){
-        console.error(err);
-        res.send("Error creating account. Username might be taken.");
+                                                                //Routes//
+
+app.get('/api/me', (req, res) =>{
+    if(req.session.userId){
+        res.json({
+            kirjautunut: true,
+            username: req.session.username
+        })
+    } else{
+        res.json({ kirjautunut: false })
     }
-});
+}) //Hallitsee istuntoa eli onko vieras/käyttäjä
 
-app.post('/login', async (req, res) =>{
-    const{ username, password } = req.body;
+app.get('/users', async (req,res) =>{
     try{
-        const result = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-        if (result.rows.length > 0){
-            const user = result.rows[0];
-            const match = await bcrypt.compare(password, user.password);
-            
-            if (match){
-                req.session.userId = user.id;
-                req.session.username = user.username;
-                return res.redirect('/index'); //
-            }
-        }
-        res.send("Invalid username or password.");
+    const result = await pool.query('SELECT * FROM users')
+    res.json(result.rows)
+  } catch (err){
+    res.status(500).json({ error: err.message })
+  }
+}) //Testi komento
+
+app.post('/kirjUlos', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) return res.status(500).send("Jotain meni pieleen!")
+        res.clearCookie('connect.sid')
+        res.redirect('/index.html')
+    })
+}) //Ulos kirjautuminen
+
+app.post('/rekisteröi', async (req, res) =>{
+    const { username, password } = req.body
+    try{
+        await registerUser(username, password)
+        res.redirect('/index.html')
     } catch (err){
         console.error(err)
-        res.status(500).send("Server error.");
+        res.send("Virhe, Käyttäjä tunnus saattaa olla käytössä.")
     }
-});
+}) //Uuden käyttäjän rekisteröinti
+
+app.post('/kirjaudu', async (req, res) =>{
+    const{ username, password } = req.body
+    try{
+        const user = await loginUser(username, password)
+        
+        if(user){
+            req.session.userId = user.id
+            req.session.username = user.username
+            return res.redirect('/index.html')
+        }
+    res.status(401).send("Käyttäjä tunnus tai salasana ei täsmää.")
+    } catch (err){
+        console.error(err)
+        res.status(500).send("Palvelin virhe :(")
+    }
+}) //Kirjautuminen jollakin tilillä
 
 
-app.listen(3001)
+
+/*
+Työn alla
+
+app.post('/kalakanta-tykkays', async(req, res) =>{ 
+    if(!req.session.userId) return res.status(500).send("Kirjaudu ensin sisään!")
+    const { imageName } = req.body
+    const userId = req.session.userId
+
+    try{
+        const checkLike = await pool.query(
+            'SELECT * FROM image_likes WHERE user_id = $1 AND image_name = $2',
+            [userId, imageName]
+        )
+
+        if (checkLike.rows.length > 0){
+            await pool.query(
+                'DELETE FROM image_likes WHERE user_id = $1 AND image_name = $2',
+                [userId, imageName]
+            )
+        } else{
+            await pool.query(
+                'INSERT INTO image_likes (user_id, image_name) VALUES ($1, $2)',
+                [userId, imageName]
+            )
+        }
+
+        res.redirect('/index')
+    } catch (err){
+        res.status(500).send("Ei onnistunut")
+    }
+
+})
+*/
+app.listen(port)
+console.log('success')
--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,26 @@
+async function kirjautunutUI(){
+    try{
+        const response = await fetch('/api/me')
+        const data = await response.json()
+
+        const vierasUI = document.getElementById('auth-vieras')
+        const käyttäjäUI = document.getElementById('auth-käyttäjä')
+        const postButton = document.getElementById('postButton')
+
+        if(data.kirjautunut){
+            vierasUI.style.display = 'none'
+            käyttäjäUI.style.display = 'block'
+        } else{
+            vierasUI.style.display = 'block'
+            käyttäjäUI.style.display = 'none'
+            postButton.style.display = 'none'
+        }
+    } catch(err){
+        console.error("auth check failed:", err)
+    }
+}
+document.addEventListener('DOMContentLoaded', kirjautunutUI) //Näyttää vieraille ja käyttäjille eri näkymät. Vois kopsata muille sivuille.
+
 const sortingButton = document.getElementById("sorting");
 const sortingItems = document.querySelectorAll(".sorting-item");
 
